43119e2d890c886d08479943d2f849174c8cf4d9
ProtoJanusLabs <165827599+ProtoJanusLabs@users.noreply.github.com>
2024-04-08 15:31:25 -0500

    Add function to delete newly added books that removes the books from array as well as front end

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 // constructor functions to create book objects
-function Book(title, author, pages, read){
+function Book(title, author, pages, read, id){
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
+    this.id = id;
 
     this.info = function(){
     console.log(`${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`);
@@ -11,18 +12,30 @@ function Book(title, author, pages, read){
 }
 
 // takes in an array to store the objects, and an event from a form to get the data to create new books
-function addBookToLibrary(event, libraryArray){
+function addBookToLibrary(event, libraryArray, id){
     let bookItem;
 
     // if the has read is true or false, change the read property as needed
     if(event.target.form[3].checked === true){
-        bookItem = new Book(event.target.form[0].value, event.target.form[1].value, event.target.form[2].value, "Has Read");
+        bookItem = new Book(event.target.form[0].value, event.target.form[1].value, event.target.form[2].value, "Has Read", id);
     }else{
-        bookItem = new Book(event.target.form[0].value, event.target.form[1].value, event.target.form[2].value, "Has Not Read");
+        bookItem = new Book(event.target.form[0].value, event.target.form[1].value, event.target.form[2].value, "Has Not Read", id);
     }
 
     libraryArray.push(bookItem);
 }
+
+function deleteBookAndRow(bookRow, libraryArray, id){
+    bookRow.remove()
+    libraryArray.forEach((item, index) => {
+        if (item.id === id) {
+            libraryArray.splice(index, 1);
+        }
+    });
+    console.log(libraryArray);
+}
+
+
 
 // render the table each time a new book is added to the array
 function render(mainBody, libraryArray){
@@ -31,6 +44,8 @@ function render(mainBody, libraryArray){
 
     libraryArray.forEach(bookItem => {
         const bookRow = document.createElement("div");
+        bookRow.classList.add("bookRow");
+        bookRow.setAttribute('id',`${bookItem.id}`);
         const titleColumn = document.createElement("div")
         titleColumn.textContent = bookItem.title;
         const authorColumn = document.createElement("div")
@@ -40,6 +55,16 @@ function render(mainBody, libraryArray){
         const readColumn = document.createElement("div")
         readColumn.textContent = bookItem.read;
         const deleteColumn = document.createElement("div")
+        deleteColumn.classList.add("deleteColumn");
+        deleteColumn.textContent = "DELETE";
+
+        bookRow.addEventListener("click", () => {
+            console.log(libraryArray);
+        })
+
+        deleteColumn.addEventListener("click", function (){
+            deleteBookAndRow(bookRow, libraryArray, bookItem.id);
+        });
 
         bookRow.appendChild(titleColumn);
         bookRow.appendChild(authorColumn);
@@ -47,18 +72,21 @@ function render(mainBody, libraryArray){
         bookRow.appendChild(readColumn);
         bookRow.appendChild(deleteColumn);
 
-        mainBody.appendChild(bookRow);
+        mainBody.appendChild(bookRow, libraryArray);
     })
 }
 
+let id = 1;
 const myLibrary = [];
 const mainContent = document.getElementById("mainContent");
 
 const submitButton = document.getElementById("submitButton");
 submitButton.addEventListener("click", function(event){
     event.preventDefault();
-    addBookToLibrary(event ,myLibrary)
-    render(mainContent, myLibrary);
+    addBookToLibrary(event ,myLibrary, id)
+    render(mainContent, myLibrary, id);
+
+    id++;
 })
 
 const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, "not read yet");
